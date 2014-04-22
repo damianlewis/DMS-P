6a9004623b980755f530aabe7d233b05e28336c8
Damian Lewis <damian@damianlewis.co.uk>
2014-04-22 11:57:25 +0100

    Added login view.

--- app/views/sessions/create.blade.php
+++ app/views/sessions/create.blade.php
@@ -1,1 +1,36 @@
-/Applications/AMPPS/www/subdomains/dms-p/app/views/sessions/create.blade.php
+@extends('layouts.login')
+
+@section('breadcrumbs')
+    <li class="current"><a href="/">Login</a></li>
+@stop
+
+@section('content')
+    <h1>Login</h1>
+
+    {{ Form::open(array('route'=>'sessions.store', 'data-abide')) }}
+        <div class="row">
+            <div class="small-12 large-6 columns">
+                {{ Form::label('username', 'Username:') }}
+                {{ Form::text('username', null, array('required')) }}
+                <small class="error">Username is required.</small>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="small-12 large-6 columns">
+                {{ Form::label('password', 'Password:') }}
+                {{ Form::password('password', array('required')) }}
+                <small class="error">Password is required.</small>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="small-12 columns">
+                <ul class="button-group radius">
+                    <li>{{ Form::submit('Login', array('class'=>'button small')) }}</li>
+                </ul>
+            </div>
+        </div>
+    {{ Form::close() }}
+
+@stop
