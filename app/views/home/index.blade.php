eb37b84a86cc647fa56a9d0b78bf6d0cebf8b6a9
Damian Lewis <damian@damianlewis.co.uk>
2014-04-23 12:46:33 +0100

    Added home page image.

--- app/views/home/index.blade.php
+++ app/views/home/index.blade.php
@@ -7,9 +7,25 @@
 @section('content')
     <h1>Welcome to DMS-P</h1>
 
-    <p>Putabam equidem satis, inquit, me dixisse. Quid igitur, inquit, eos responsuros putas? Neque solum ea communia, verum etiam paria esse dixerunt. Eadem fortitudinis ratio reperietur. Duo Reges: constructio interrete. Igitur neque stultorum quisquam beatus neque sapientium non beatus. Experiamur igitur, inquit, etsi habet haec Stoicorum ratio difficilius quiddam et obscurius. Non dolere, inquam, istud quam vim habeat postea videro; Eadem nunc mea adversum te oratio est. Sed in rebus apertissimis nimium longi sumus.</p>
+    <div class="row">
+        <div class="small-12 columns">
+            <img data-interchange="[/img/photo-0041-small.png, (small)], [/img/photo-0041.png, (large)]">
+        </div>
+    </div>
+
+    <hr>
     
-    @unless (Auth::check())
-        <a href="{{ route('login') }}" class="button radius">Login</a>
-    @endunless
+    <div class="row">
+        <div class="small-12 columns">
+            <p>Putabam equidem satis, inquit, me dixisse. Quid igitur, inquit, eos responsuros putas? Neque solum ea communia, verum etiam paria esse dixerunt. Eadem fortitudinis ratio reperietur. Duo Reges: constructio interrete. Igitur neque stultorum quisquam beatus neque sapientium non beatus. Experiamur igitur, inquit, etsi habet haec Stoicorum ratio difficilius quiddam et obscurius. Non dolere, inquam, istud quam vim habeat postea videro; Eadem nunc mea adversum te oratio est. Sed in rebus apertissimis nimium longi sumus.</p>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="small-12 columns">
+            @unless (Auth::check())
+                <a href="{{ route('login') }}" class="button radius">Login</a>
+            @endunless
+        </div>
+    </div>
 @stop
