c06663cbb8f4c13a8b773871d0af922c2a888e27
Damian Lewis <damian@damianlewis.co.uk>
2014-07-10 12:57:40 +0100

    Removed unnecessary and redundant configuration details.

--- app/config/local/app.php
+++ app/config/local/app.php
@@ -3,20 +3,9 @@
 return array(
 
 	'debug' => true,
-	'url' => 'http://dms-p.local',
     'timezone' => 'Europe/London',
+    
     'providers' => array(
         'Way\Generators\GeneratorsServiceProvider'
-    ),
-    
-    // 'providers' => array(
-    //     'Way\Generators\GeneratorsServiceProvider',
-    //     'Cartalyst\Sentry\SentryServiceProvider'
-    // ),
-
-    // 'aliases' => array(
-    //     'Sentry'    => 'Cartalyst\Sentry\Facades\Laravel\Sentry',
-    //     'Carbon'    => 'Carbon\Carbon'
-    // )
-
+    )
 );
