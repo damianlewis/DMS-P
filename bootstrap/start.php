f73fd40af47351ec3c58295d20f04be2f2015b9a
Damian Lewis <damian@damianlewis.co.uk>
2014-07-10 12:49:56 +0100

    Added environment detection.

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -24,11 +24,14 @@ $app = new Illuminate\Foundation\Application;
 |
 */
 
-$env = $app->detectEnvironment(array(
-
-	'local' => array('Damians-iMac.local'),
-
-));
+$env = $app->detectEnvironment(function()
+{
+    if (file_exists(__DIR__ . '/environment.php')) {
+        return require __DIR__.'/environment.php';
+    } else {
+        return 'production';
+    }
+});
 
 /*
 |--------------------------------------------------------------------------
