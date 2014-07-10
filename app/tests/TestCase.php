4fbd899978ecb09b4f35fdda6167a1018e717c7e
Damian Lewis <damian@damianlewis.co.uk>
2014-07-10 12:47:16 +0100

    Removed unit testing.

--- app/tests/TestCase.php
+++ app/tests/TestCase.php
@@ -3,15 +3,6 @@
 class TestCase extends Illuminate\Foundation\Testing\TestCase {
 
     /**
-     * Default preparation for each test
-     */
-    public function setUp()
-    {
-        parent::setUp();
-        $this->prepareForTests();
-    }
-
-    /**
      * Creates the application.
      *
      * @return \Symfony\Component\HttpKernel\HttpKernelInterface
@@ -23,14 +14,6 @@ class TestCase extends Illuminate\Foundation\Testing\TestCase {
         $testEnvironment = 'testing';
 
         return require __DIR__.'/../../bootstrap/start.php';
-    }
-
-/**
-     * Migrate the database
-     */
-    private function prepareForTests()
-    {
-        Artisan::call('migrate');
     }
 
 }
